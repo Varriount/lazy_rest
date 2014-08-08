9b5f957732c05c7d90f689bcca45dbc13f2a2c0d
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-08-08 22:25:01 +0200

    Adds task to run tests, disables recursive include. Refs #1.

--- nakefile.nim
+++ nakefile.nim
@@ -1,5 +1,10 @@
 import nake, os, times, osproc, md5, lazy_rest, sequtils, json, posix, strutils
 
+type
+  Failed_test = object of EAssertionFailed ## \
+    ## Indicates something failed, with error output if `errors` is not nil.
+    errors*: string
+
 template glob_rst(basedir: string = nil): expr =
   ## Shortcut to simplify getting lists of files.
   ##
@@ -24,6 +29,19 @@ iterator all_rst_files(): tuple[src, dest: string] =
     r.dest = rst_name.change_file_ext("html")
     yield r
 
+
+proc test_shell(cmd: varargs[string, `$`]): bool {.discardable.} =
+  ## Like direShell() but doesn't quit, rather raises an exception.
+  let
+    full_command = cmd.join(" ")
+    (output, exit) = full_command.exec_cmd_ex
+  result = 0 == exit
+  if not result:
+    var e = new_exception(Failed_test, "Error running " & full_command)
+    e.errors = output
+    raise e
+
+
 proc doc(open_files = false) =
   # Generate html files from the rst docs.
   for rst_file, html_file in all_rst_files():
@@ -36,7 +54,9 @@ proc doc(open_files = false) =
 
   echo "All docs generated"
 
+
 proc doco() = doc(true)
+
 
 proc validate_doc() =
   for rst_file, html_file in all_rst_files():
@@ -54,12 +74,32 @@ proc clean() =
       path.removeFile()
   echo "Temporary files cleaned"
 
+
 proc install_babel() =
   direshell("babel install -y")
   echo "Installed"
+
+
+proc run_tests() =
+  var failed: seq[string] = @[]
+  for test_file in walk_files("tests/*/test*nim"):
+    let (dir, name, ext) = test_file.split_file
+    with_dir test_file.parent_dir:
+      try:
+        echo "Testing ", name
+        test_shell("nimrod c -r", name)
+      except Failed_test:
+        failed.add(test_file)
+  if failed.len > 0:
+    echo "Uh oh, " & $failed.len & " tests failed running"
+    for f in failed: echo "\t" & failed
+  else:
+    echo "All tests run without errors."
+
 
 task "check_doc", "Validates rst format with python.": validate_doc()
 task "clean", "Removes temporal files, mostly.": clean()
 task "doc", "Generates HTML docs.": doc()
 task "doco", "Like 'doc' but also calls 'open' on generated HTML.": doco()
 task "i", "Uses babel to force install package locally.": install_babel()
+task "test", "Runs local generation tests.": run_tests()
